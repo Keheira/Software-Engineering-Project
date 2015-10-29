65cae2246b6c08d9bc3b776a28d158a7f81cb0c1
Keheira <keheirah@gmail.com>
2015-10-29 15:27:24 -0400

    interation 2

--- listen.js
+++ listen.js
@@ -1,15 +1,19 @@
+//Firebase Reference
+var db = new Firebase('https://user-stories.firebaseio.com/');
+
 //when tab opens get tab
 chrome.tabs.getSelected(null, function(tab){
-	var stime = ;
+	var tabURL = tab.url;
+	//Save data to Firebase
+	db.push({url:tabURL});
 });
-//Firebase Reference
-var db = new Firebase
 
-//listen for tab close
-if(){
-	var url = ;
-	var ftime = ;
 
-	//save data to firebase
-	db.push({url:url, time:time});
-}
+// //listen for tab close
+// if(){
+// 	var url = ;
+// 	var ftime = ;
+//
+// 	//save data to firebase
+// 	db.push({url:url, time:time});
+// }
