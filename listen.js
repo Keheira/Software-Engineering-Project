8fb7fe0d07025c93c48f014c1dc5724b8ec38c65
Keheira <keheirah@gmail.com>
2015-10-28 21:32:04 -0400

    iteration 1

---
+++ listen.js
@@ -0,0 +1,15 @@
+//when tab opens get tab
+chrome.tabs.getSelected(null, function(tab){
+	var stime = ;
+});
+//Firebase Reference
+var db = new Firebase
+
+//listen for tab close
+if(){
+	var url = ;
+	var ftime = ;
+
+	//save data to firebase
+	db.push({url:url, time:time});
+}
