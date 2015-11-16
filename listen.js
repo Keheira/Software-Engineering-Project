ac2cd074f2a02ebc25b446d91be757ad9e7c778c
Keheira <keheirah@gmail.com>
2015-11-16 12:20:59 -0500

    New update

--- listen.js
+++ listen.js
@@ -1,15 +1,33 @@
 //Firebase Reference
 var db = new Firebase('https://user-stories.firebaseio.com/');
 
-//when tab opens get tab
-chrome.tabs.getSelected(null, function(tab){
-	var tabURL = tab.url;
-	//Save data to Firebase
-	db.push({url:tabURL});
+var activeTab;
+function updateActive(tab){
+	activeTab = tab;
+}
+function onActivated(info){
+	chrome.tabs.get(info.tabId, updateActive);
+}
+function onUpdated(info, tab){
+	if(tab.active)
+	db.push({url:tab.url,active_time:time});
+		updateActive(tab);
+}
+function.tabs.query({active: true, lastFocusedWindow}, function(tabs){
+	updateActive(tabs[0]);
+	chrome.tabs.onActivated.addListerner(onActivated);
+	chrome.tabs.onUpdated.addListerner(onUpdated);
 });
 
+//when tab opens get tab
+// chrome.tabs.getSelected(null, function(tab){
+// 	var tabURL = tab.url;
+// 	//Save data to Firebase
+// 	db.push({url:tabURL});
+// });
 
-// //listen for tab close
+
+//listen for active tab
 // if(){
 // 	var url = ;
 // 	var ftime = ;
