1255f610bd02f13d3f2a81b09adcc540ce287c36
Keheira <keheirah@gmail.com>
2015-12-02 01:26:22 -0500

    Still working on 96% product

--- listen.js
+++ listen.js
@@ -1,49 +1,48 @@
 //Firebase Reference
 var db = new Firebase('https://user-stories.firebaseio.com/');
+var time = null;
+var tabs = null;
+var interval = setInterval(timer,1000);
+var date;
 
-var interval = null;
-var updateTime = 5000;
-var currentTabInfo = {};
-var userActive = true;
-// var tabURL;
-// var activeTab;
-// function updateActive(tab){
-// 	activeTab = tab;
-// }
-// function onActivated(info){
-// 	chrome.tabs.get(info.tabId, updateActive);
-// }
-// function onUpdated(info, tab){
-// 	if(tab.active)
-// 		updateActive(tab);
-// }
-// function.tabs.query({active: true, lastFocusedWindow}, function(tabs){
-// 	updateActive(tabs[0]);
-// 	chrome.tabs.onActivated.addListerner(onActivated);
-// 	chrome.tabs.onUpdated.addListerner(onUpdated);
-// });
+function timer(){
+	date = new Date();
+	document.getElementById('time').innerHTML = date.toLocaleTimeString();
+}
+
+chrome.windows.getAll({populate:true},function(windows){
+  windows.forEach(function(window){
+    window.tabs.forEach(function(tab){
+      //collect all of the urls here, I will just log them instead
+      console.log(tab.url);
+    });
+  });
+});
+
+var getTabs = function() {
+	d = new Date();
+};
+
+var resetVariables = function(){
+	time = null;
+	tabs = null;
+	clearInterval(time);
+}
 
 chrome.tabs.query({
 	active: true,
-	currentWindow: true
-}, function(tabs){
-	getURL(tabs[0].url);
+	currentWindow: true,
+},function(tab){
+	// console.log("working..");
+	// if(tabs === null) tabs = tab.url;
+	// if(tabs != tab.url){
+		// var newDate = new Date();
+		// var record = newDate - d;
+		// console.log("push!!!!");
+		db.push({header:tab.title,url:tab.url, time: date.toLocaleTimeString()});
+		// tabs = tab.url;
+	// }
+	// console.log("I got the tab!" + tab.url);
 });
-
-// when tab opens get tab
-// chrome.tabs.getSelected(null, function(tab){
-// 	tabURL = tab.url;
-// 	var d = new Date();
-// 	//Save data to Firebase
-// 	db.push({url:tabURL});
-// });
-
-
-//listen for active tab
-// if(){
-// 	var url = ;
-// 	var ftime = ;
-//
-// 	//save data to firebase
-// 	db.push({url:url, time:time});
-// }
+chrome.windows.onRemoved.addListener(resetVariables);
+chrome.windows.onCreated.addListener(getTabs);
---
+++ newtab.js
@@ -0,0 +1,17 @@
+var ref = new Firebase('https://user-stories.firebaseio.com/');
+    // Retrieve new posts as they are added to our database
+    ref.on("child_added", function(snapshot, prevChildKey) {
+        var newPost = snapshot.val();
+        var table = document.getElementById("results");
+        var row = table.insertRow(1);
+        // var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(0);
+        var cell3 = row.insertCell(1);
+        // cell1.innerHTML = newPost.header;
+        cell2.innerHTML = newPost.url;
+        cell3.innerHTML = newPost.time;
+        console.log("title: " + newPost.header);
+        console.log("url: " + newPost.url);
+        console.log("Time: " + newPost.time);
+        console.log("Previous Post ID: " + prevChildKey);
+    });
