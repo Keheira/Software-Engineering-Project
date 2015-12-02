55be3fae48661bba17e73448d574ec65c0995496
Keheira <keheirah@gmail.com>
2015-12-02 02:57:02 -0500

    Presentation Ready

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Software-Engineering-Project
-project created for my COMP 510 class
+project created for my COMP 510 class. We were motivated to create this product from our teacher Dr. Roy. He challenged us to create a way to tracker a users time online.
 
 ##Team Members
-<a href=""></a>
-<a href=""></a>
+<a href="">Keheira</a>
+<a href="">Alex</a>
 
 ##Refrences
 <a href="https://github.com/srikarg/Trackr.git">Trackr by srikarg</a>
@@ -13,6 +13,13 @@ project created for my COMP 510 class
 	<li>Chrome Extension</li>
 	<li>Firebase</li>
 	<li>Github/Git</li>
-	<li></li>
+	<li>HTML/CSS/JS</li>
+</ul>
+
+##Future Additions
+<ul>
+	<li>Better way to view results</li>
+	<li>Working timer</li>
+	<li>Complete use in the background</li>
 </ul>
 
--- listen.js
+++ listen.js
@@ -1,7 +1,7 @@
 //Firebase Reference
 var db = new Firebase('https://user-stories.firebaseio.com/');
-var time = null;
-var tabs = null;
+// var time = null;
+// var tabs = null;
 var interval = setInterval(timer,1000);
 var date;
 
@@ -10,39 +10,44 @@ function timer(){
 	document.getElementById('time').innerHTML = date.toLocaleTimeString();
 }
 
-chrome.windows.getAll({populate:true},function(windows){
-  windows.forEach(function(window){
-    window.tabs.forEach(function(tab){
-      //collect all of the urls here, I will just log them instead
-      console.log(tab.url);
-    });
-  });
-});
+//personal reference
+// chrome.windows.getAll({populate:true},function(windows){
+//   windows.forEach(function(window){
+//     window.tabs.forEach(function(tab){
+//       console.log(tab.url);
+//     });
+//   });
+// });
 
-var getTabs = function() {
-	d = new Date();
-};
+// var getTabs = function() {
+// 	d = new Date();
+// };
 
-var resetVariables = function(){
-	time = null;
-	tabs = null;
-	clearInterval(time);
-}
-
-chrome.tabs.query({
-	active: true,
-	currentWindow: true,
-},function(tab){
+// var resetVariables = function(){
+// 	time = null;
+// 	tabs = null;
+// 	clearInterval(time);
+// }
+chrome.tabs.getSelected(null, function(tab){
+	var tabURL = tab.url;
+	console.log('This works');
+	//Save data to Firebase
+	db.push({url:tabURL});
+ });
+// chrome.tabs.query({
+// 	active: true,
+// 	currentWindow: true,
+// },function(tab){
 	// console.log("working..");
 	// if(tabs === null) tabs = tab.url;
 	// if(tabs != tab.url){
 		// var newDate = new Date();
 		// var record = newDate - d;
 		// console.log("push!!!!");
-		db.push({header:tab.title,url:tab.url, time: date.toLocaleTimeString()});
+		// db.push({url:tab.url, time: date.toLocaleTimeString()});
 		// tabs = tab.url;
 	// }
 	// console.log("I got the tab!" + tab.url);
-});
-chrome.windows.onRemoved.addListener(resetVariables);
-chrome.windows.onCreated.addListener(getTabs);
+// });
+// chrome.windows.onRemoved.addListener(resetVariables);
+// chrome.windows.onCreated.addListener(getTabs);
